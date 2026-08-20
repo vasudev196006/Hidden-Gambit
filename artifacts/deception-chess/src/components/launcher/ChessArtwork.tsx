f2919a8d4166f834d2b89b35f49e0b94bdcfa6b3
vasudev196006 <s24rt513@vjcet.org>
2026-08-20 20:35:54 +0530

    feat: use mobile_ui.png as mobile background artwork

--- artifacts/deception-chess/src/components/launcher/ChessArtwork.tsx
+++ artifacts/deception-chess/src/components/launcher/ChessArtwork.tsx
@@ -7,18 +7,31 @@ export const ChessArtwork: React.FC = () => {
       <div className="absolute inset-0 bg-[#050505]" />
 
       {/* Desktop Background Image (desktopbg.png) */}
-      <div className="relative w-full h-full flex items-center justify-center">
+      <div className="relative w-full h-full hidden md:flex items-center justify-center">
         <img
           src="/desktopbg.png"
           alt="Desktop Background"
           className="w-full h-full object-cover object-center opacity-100 transition-opacity duration-700 filter contrast-[1.05] brightness-[1.02]"
         />
 
-        {/* Ambient Dark Edge Vignettes to blend seamlessly */}
+        {/* Ambient Dark Edge Vignettes */}
         <div className="absolute inset-x-0 top-0 h-20 bg-gradient-to-b from-[#050505] via-[#050505]/40 to-transparent" />
         <div className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-[#050505] via-[#050505]/40 to-transparent" />
         <div className="absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-[#050505] to-transparent" />
         <div className="absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-[#050505] to-transparent" />
+      </div>
+
+      {/* Mobile Background Image (mobile_ui.png) */}
+      <div className="relative w-full h-full flex md:hidden items-center justify-center">
+        <img
+          src="/mobile_ui.png"
+          alt="Mobile Background"
+          className="w-full h-full object-cover object-center opacity-100 filter contrast-[1.05] brightness-[1.02]"
+        />
+
+        {/* Mobile Edge Vignettes */}
+        <div className="absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-[#050505] via-[#050505]/30 to-transparent" />
+        <div className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-[#050505] via-[#050505]/40 to-transparent" />
       </div>
     </div>
   );
